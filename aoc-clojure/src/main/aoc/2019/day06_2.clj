f97995fa122c2f58d9c136c9e4bf55741535ce11
sooheon <sooheon.k@gmail.com>
2019-12-06 18:08:53 +0800

    Add benchmarks

--- aoc-clojure/src/main/aoc/2019/day06_2.clj
+++ aoc-clojure/src/main/aoc/2019/day06_2.clj
@@ -1,6 +1,7 @@
 (ns aoc.2019.day06-2
   (:require [aoc.2019.day06 :as day06]
-            [aoc.utils :as u]))
+            [aoc.utils :as u]
+            [criterium.core :as crit]))
 
 (def i (day06/read-pairs (u/input 2019 6)))
 (def ->parent (into {} (map (comp vec reverse) i)))
@@ -13,12 +14,17 @@
       anc)))
 
 ;; pt1
-(->> (for [n nodes]
-       (count (ancestors ->parent n)))
-     (apply +))
+(comment
+ (crit/quick-bench
+  (->> (for [n nodes]
+         (count (ancestors ->parent n)))
+       (reduce +))))
 
 ;; pt2
-(let [a (ancestors ->parent "YOU")
-      b (ancestors ->parent "SAN")]
-  (+ (count (take-while (complement (set b)) a))
-     (count (take-while (complement (set a)) b))))
+(comment
+ (crit/quick-bench
+  (let [a (ancestors ->parent "YOU")
+        b (ancestors ->parent "SAN")]
+    (+ (count (take-while (complement (set b)) a))
+       (count (take-while (complement (set a)) b))))))
+
