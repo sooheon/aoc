1b026e75dc4677812d4f9b2698b38b2e8d216d9f
sooheon <sooheon.k@gmail.com>
2019-12-11 16:01:41 +0800

    Day 11

--- aoc-clojure/src/main/aoc/2019/day11.clj
+++ aoc-clojure/src/main/aoc/2019/day11.clj
@@ -6,62 +6,61 @@
 
 (def movement [[0 -1] [1 0] [0 1] [-1 0]])        ;; n, e, s, w
 
-(defn new-heading [heading command]
-  (let [new-h (-> (case command
-                    0 (dec heading)
-                    1 (inc heading))
-                  (mod 4))]
-    new-h))
+(defn new-heading
+  "NESW -> 0123"
+  [heading command]
+  (-> (case command
+        0 (dec heading)
+        1 (inc heading))
+      (mod 4)))
 
 (defn move [position heading]
   (mapv + position (movement heading)))
-
-(defn color [{:keys [position board]}]
-  (get-in board (reverse position) 0))
 
 (defn paint-and-move
   [{:keys [position heading] :as hull} new-color dir-command]
   (let [new-h (new-heading heading dir-command)]
     (-> hull
-        (assoc-in (cons :board (reverse position)) new-color)
+        (assoc-in (cons :board position) new-color)
+        (update :visited conj position)
         (update :position move new-h)
-        (assoc :heading new-h)
-        (update :visited conj position))))
+        (assoc :heading new-h))))
 
-(defn draw [{:keys [board] :as hull}]
-  (let [ys (keys board)
-        xs (mapcat keys (vals board))]
-    (for [row (range (apply min ys) (inc (apply max ys)))]
-      (println
-       (str/join
-        (map #(case (get-in board [row %] 0)
-                0 \.
-                1 \#)
-             (range (apply min xs) (inc (apply max xs)))))))))
+(defn color [{:keys [position board]}]
+  (get-in board position 0))
 
 (defn run-painter
-  ([program] (run-painter program {}))
-  ([program hull]
-   (let [painter (ic/init program)
-         result (ic/run painter)
-         io (a/go-loop [hull (merge {:position [0 0]
-                                     :heading 0
-                                     :visited #{}
-                                     :board {}} hull)]
-              (>! (:in painter) (color hull))
-              (let [new-color (<! (:out painter))
-                    dir-command (<! (:out painter))]
-                (if (and new-color dir-command)
-                  (recur (paint-and-move hull new-color dir-command))
-                  hull)))]
-     [result io])))
+  ([prog-str] (run-painter prog-str {}))
+  ([prog-str hull]
+   (let [painter (ic/init prog-str)]
+     (ic/run painter)
+     (a/go-loop [hull (merge {:position [0 0]
+                              :heading 0
+                              :visited #{}
+                              :board {}} hull)]
+       (>! (:in painter) (color hull))
+       (let [new-color (<! (:out painter))
+             dir-command (<! (:out painter))]
+         (if (and new-color dir-command)
+           (recur (paint-and-move hull new-color dir-command))
+           hull))))))
+
+(defn draw [{:keys [board]}]
+  (let [xs (keys board)
+        ys (mapcat keys (vals board))
+        [x-range y-range] (map #(range (apply min %)
+                                       (inc (apply max %)))
+                               [xs ys])]
+    (for [row y-range]
+      (println
+       (str/join (map #(case (get-in board [% row] 0)
+                         0 \space
+                         1 \#)
+                      x-range))))))
 
 (comment
  ;; part 1
- (let [[res io] (run-painter (u/input 2019 11))]
-   (count (:visited (<!! io))))
+ (count (:visited (<!! (run-painter (u/input 2019 11)))))
 
  ;; part 2
- (let [[res io] (run-painter (u/input 2019 11)
-                             {:board {0 {0 1}}})]
-   (draw (<!! io))))
+ (draw (<!! (run-painter (u/input 2019 11) {:board {0 {0 1}}}))))
